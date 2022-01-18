c0b722bbdfb76c832a7ffe7667dba6e71a74b1de
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:47:25 +0300

    pom update

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,20 +3,22 @@ package app.model;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cat extends Animal {
+public class Dog extends Animal {
     private String sound;
 
-    public Cat(String sound) {
+    public Dog(String sound) {
         this.sound = sound;
     }
 
     @Override
     void say() {
-        System.out.println("I am a Cat" + sound);
+        System.out.println("Dog says " + sound);
     }
 
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "Dog{" +
+                "sound='" + sound + '\'' +
+                '}';
     }
 }
